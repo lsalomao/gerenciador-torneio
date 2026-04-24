30a016e71ed32cf00fee9d07f97fbb479648e5dd
Leandro Salomao <ledsalomao@gmail.com>
2026-04-24 16:26:20 -0300

    MelhoriaLayout dashboard

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=True)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost'])
+ALLOWED_HOSTS = ["torneio.ledtech.app", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
